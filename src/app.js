baa153dc81834525bb6566050e0cac52b5dcf171
luccamicale <luccamicale2015@gmail.com>
2022-11-01 22:07:33 -0300

    complete the parts of components

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = '';
+  }
+
+  render() {
+    return <Calculator />;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/calculator';
+import Calculator from './components/calculator';
+import './components/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
 );
