64b5af4d4071f335794e21211ec489966ca3277e
luccamicale <luccamicale2015@gmail.com>
2022-11-03 13:56:07 -0300

    Initial Commit

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -39,7 +39,7 @@ class Calculator extends React.Component {
           <button onClick={this.onClickHandler} type="button" name="4">4</button>
           <button onClick={this.onClickHandler} type="button" name="5">5</button>
           <button onClick={this.onClickHandler} type="button" name="6">6</button>
-          <button onClick={this.onClickHandler} type="button" id="less">-;</button>
+          <button onClick={this.onClickHandler} type="button" id="less">-</button>
           <button onClick={this.onClickHandler} type="button" name="1">1</button>
           <button onClick={this.onClickHandler} type="button" name="2">2</button>
           <button onClick={this.onClickHandler} type="button" name="3">3</button>
