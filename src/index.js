3f7341a1f0ec11225622ff43b3837922a05fec70
luccamicale <luccamicale2015@gmail.com>
2023-11-06 16:56:24 -0300

    fix: :art: I modified the styles and texts in the website

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './modules-css/nav.css';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,24 @@
 import '../modules-css/home.css';
+import calculadoranew from '../icons/calculadoranew.png'
 
 const Home = () => (
   <div className="main-cont">
     <div className="home-cont">
       <h3 className="welcome">Welcome to our Page!</h3>
-      <p className="paragraph-one">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint dolor cupidatat
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint dolor cupidatat
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      <p style={{width: '50%',marginLeft: '40%', border: '1px solid ', padding: '25px', borderRadius: '16px', boxShadow: '5px 5px 5px 5px', backgroundColor: '#faf0ca'}}>
+        "Math Magicians" typically refers to individuals who possess exceptional 
+        mathematical skills and are capable of 
+        performing complex mathematical calculations and problem-solving with ease and precision.
+        These 
+        individuals are often admired for their ability to manipulate numbers and equations as if 
+        they have a magical touch when it comes to mathematics.
       </p>
-      <p className="paragragh-two">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint dolor cupidatat
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint dolor cupidatat
-        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur.Duis aute irure reprehenderit
-        in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      <img src={calculadoranew} style={{marginLeft: '55px', marginTop: '-35px'}} alt='calculator' width={'250px'}></img>
+      <p  style={{width: '50%',marginLeft: '40%', border: '1px solid ', padding: '25px', borderRadius: '16px', boxShadow: '5px 5px 5px 5px', backgroundColor: '#faf0ca', marginTop: '-180px'}}>
+        It's important to note that while some individuals may have an innate talent for mathematics, mathematical
+        proficiency can also be developed and honed through education, practice, and dedication.
+        Not all Math Magicians are born with their abilities; some have acquired their skills 
+        through hard work and dedication to the subject.
       </p>
     </div>
   </div>
