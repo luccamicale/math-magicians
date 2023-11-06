b92ba13ee89c3aae7d1984e99ffa5c11519a25d2
luccamicale <luccamicale2015@gmail.com>
2023-11-06 17:54:38 -0300

    fix: :art: I fixed a linters

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import './modules-css/nav.css';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
